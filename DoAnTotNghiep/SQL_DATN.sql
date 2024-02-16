43921a3ad7ed99ec2aaa7403cce12e6b16b0c70f
VietAnh <VietAnh>
2024-02-16 21:16:13 +0700

    SQL

---
+++ DoAnTotNghiep/SQL_DATN.sql
@@ -0,0 +1,29 @@
+Create database VAHRM
+
+CREATE TABLE Users
+(
+    Id VARCHAR(32) PRIMARY KEY,
+    UserName VARCHAR(256),
+    Password VARCHAR(256),
+    IsAdmin bit,
+	Permission VARCHAR(max),
+	PermissionGroup VARCHAR(max),
+);
+ALTER TABLE Users ADD Name NVARCHAR(255);
+ALTER TABLE Users ADD Code VARCHAR(255);
+ALTER TABLE Users ADD Gender VARCHAR(255);
+ALTER TABLE Users ADD IdentityNumber VARCHAR(255);
+ALTER TABLE Users ADD DateOfBirth Datetime;
+ALTER TABLE Users ADD PhoneHouseholder VARCHAR(255);
+ALTER TABLE Users ADD Email VARCHAR(255);
+ALTER TABLE Users ADD Address NVARCHAR(255);
+ALTER TABLE Users ADD Salary int;
+
+
+CREATE TABLE PermissionGroup
+(
+    Id VARCHAR(32) PRIMARY KEY,	
+	CodeGroup VARCHAR(256),
+	NameGroup VARCHAR(256),
+	Permission VARCHAR(max),
+);
